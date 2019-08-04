880275886605df129d32db5419faacac0a98e000
Elias <eliasrobotics@gmail.com>
2019-08-04 15:31:47 +0200

    print shape

--- Input/urmp_input.py
+++ Input/urmp_input.py
@@ -181,6 +181,7 @@ class URMPInput(object):
 
         # dataset = self.mean_imputer.fit_transform(dataset)
         def filter_fn(x):
+            print("######################################## ", x, x.shape)
             contains_nan = np.isnan(x)
             if contains_nan:
                 print("###################################nano dimmerda")
